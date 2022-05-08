8d4f7ece088c30df0272ac52205df4c95c6d4d46
azkiyas <safiraazkiya@gmail.com>
2022-05-08 18:59:58 +0700

    Revisi script sampling.sh

--- sampling.sh
+++ sampling.sh
@@ -4,17 +4,17 @@
 wget -P ./data https://github.com/labusiam/dataset/raw/main/weather_data.xlsx
 
 # Convert setiap sheet
-in2csv weather_data.xlsx --sheet "weather_2014" > weather_2014.csv
-in2csv weather_data.xlsx --sheet "weather_2015" > weather_2015.csv
+in2csv ./data/weather_data.xlsx --sheet "weather_2014" > data/weather_2014.csv
+in2csv ./data/weather_data.xlsx --sheet "weather_2015" > data/weather_2015.csv
 
 # Menggabungkan data weather 2014 dan 2015
-csvstack weather_2014.csv weather_2015.csv > weather.csv
+csvstack data/weather_2014.csv data/weather_2015.csv > data/weather.csv
 
 # Hapus file 
-rm ./data/weather_data.csv
+rm ./data/weather_data.xlsx
 
 # Melakukan sampling
-cat weather.csv | sample -r 0.3 weather.csv > sample_weather.csv
+cat ./data/weather.csv | sample -r 0.3 > data/sample_weather.csv
 
 
 
